32ff72d319bfc5b3a1c4bed904631ca238121119
Chiara Todesco <chitodesco@gmail.com>
2020-11-12 16:37:48 +0100

    implementation reviews and favourites

--- modules/favourites.js
+++ modules/favourites.js
@@ -1,3 +1,21 @@
-module.exports = {
+const { query } = require('express');
+const { Component } = require('react');
+const db = require('./db');
+let connection;
 
+module.exports = {
+    //Query  per leggere tutti i preferiti di un utente
+    getFavouritesByUserNickname: async (nickname) =>{
+        queryFavouritesUser = await db.query('SELECT media_title, media_publishing_date FROM favourites ' + 
+        'WHERE user_nickname=?', [nickname]);
+        return queryFavouritesUser;
+    },
+    
+    //Query per creare nuovo favorito per l'utente
+    setNewFavourite: async (nickname, title, publishing_date)  =>{
+        await db.query('INSERT INTO favourites (user_nickname, media_title, media_publishing_date)'+
+        ' VALUES(?, ?, ?) ', [nickname, title, publishing_date]);
+        favouritesUser = favourites.getFavouritesByUserNickname(nickname);
+        return favouritesUser;             
+    }
 }
