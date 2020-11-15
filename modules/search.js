7abd03174ee00357c25edfc08370635ed8bbe413
Chiara Todesco <chitodesco@gmail.com>
2020-11-15 10:51:56 +0100

    solved merge errors

--- modules/search.js
+++ modules/search.js
@@ -1,4 +1,88 @@
-module.exports = {
+const db = require('./db_manager');
 
+module.exports = {
+    //Query di ricerca, options è usato per indicare molteplici filtri da applicare alla ricerca 
+    getMediasByOptions: async (options) =>{ //unsafeOptions 
+        //const defaults = {genre: "void", title: "void", quality: "void", price: {value:"void", operation: "void"}, 
+        //views_count:{value:"void", operation: "void"}, director_name: "void", actors: "void"};
+        //const options = Object.assign({},defaults, unsafeOptions);
+        let conditions = "";
+        console.log(options);
+
+        //if (options.genre === "void") 
+        //if (typeof (options.genre) == 'undefined') 
+        //if (options.genre === undefined)
+        //if (!("genre" in options))  
+        if (options.genre === undefined) {
+            conditions += " AND genre=genre";
+        }else {
+            condition += " AND genre=" + db.escape(options.genre);
+        }
+
+        if (options.title === undefined){
+            conditions += " AND title=title";
+        }else {
+            conditions += " AND title=" + db.escape(options.title);
+        }
+
+        if (options.quality === undefined){
+            conditions += " AND quality=quality";
+        }else {
+            conditions += " AND quality=" + db.escape(options.quality);
+        }
+
+        if (options.price.value === undefined){
+            conditions += " AND price=price";
+        }else {
+            switch (options.price.operation){
+                case 0:
+                    conditions += " AND price=" + db.escape(options.price.value);
+                    break;
+                case 1:
+                    conditions += " AND price<=" + db.escape(options.price.value);
+                    break;
+                case 2:
+                    conditions += " AND price>=" + db.escape(options.price.value);
+                    break;
+                default:
+                    throw "Wrong option choice. Please choose another option";
+            }
+            
+        }
+
+        if (options.views_count.value === undefined){
+            conditions += " AND views_count=views_count";
+        }else {
+            switch (options.views_count.operation){
+                case 0:
+                    conditions += " AND views_count=" + db.escape(options.views_count.value);
+                    break;
+                case 1:
+                    conditions += " AND views_count<=" + db.escape(options.views_count.value);
+                    break;
+                case 2:
+                    conditions += " AND views_count>=" + db.escape(options.views_count.value);
+                    break;
+                default:
+                    throw "Wrong option choice. Please choose another option";
+            }
+        }
+
+        if (options.director_name === undefined){
+            conditions += " AND director_name=director_name";
+        }else {
+            conditions += " AND director_name LIKE %" + db.escape(options.director_name) + "%"; 
+        }
+
+        if(options.actors === undefined){
+            conditions += " AND actors=actors";
+        }else {
+            conditions += " AND actors LIKE %" + db.escape(options.actors) + "%";
+        }
+        
+        let querySearch = await db.query('SELECT * FROM media WHERE available=true' + conditions, []);
+        return querySearch;
+    }
+    
     
 }
--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 (async () => {
-    const Media = require('./modules/media');
+    const search = require("./modules/search")
     const db_manager = require('./modules/db_manager')
-    db_manager.connect("progetto", "", "progetto_ennova_umana", '192.168.64.2');
-    let a = await Promise.all([Media.getMostPopularMedias(), Media.getTopMediasByGenre('ChiaraT92'), Media.getNewReleases()]);
-    let obj = {
-        popular: a[0],
-        topByGenre: a[1],
-        newReleases: a[2]
-    }
+
+    db_manager.connect("root", "", "progetto_ennova_umana");
+    console.log(await search.getMediasByOptions({title:"Tolo Tolo"}));
 })()
+
+
+//{genre: "void", title: "Tolo Tolo", quality: "void", price: {value:"void", operation: "void"}, 
+//views_count:{value:"void", operation: "void"}, director_name: "void", actors: "void"}
